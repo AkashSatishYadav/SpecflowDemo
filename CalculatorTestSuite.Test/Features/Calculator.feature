b1b01d982794ad3e61cda55a705512b202a50043
Akash Yadav <akashy211@gmail.com>
2023-05-19 15:40:40 +0530

    specflow proj

---
+++ CalculatorTestSuite.Test/Features/Calculator.feature
@@ -0,0 +1,40 @@
+﻿Feature: Calculator
+		 1. Add
+		 2. Substract
+		 3. Multiply
+
+To test all functionality of a calculator
+
+@Cal
+Scenario: Addition of two number
+	Given the first number is <firstNum>
+	When the second number is <secondNum>
+	Then the sum of first and second should be <addResult>
+
+Examples: 
+		| firstNum | secondNum | addResult |
+		| 4        | 5         | 9         |
+		| 100      | 5         | 105       |
+		| 1000     | 1000      | 2000      |
+
+Scenario: Substraction of two number
+	Given the first number is <firstNum>
+	When the second number is <secondNum>
+	Then the substraction of first and second should be <subsResult>
+
+Examples: 
+		| firstNum | secondNum | subsResult |
+		| 4        | 5         | -1         |
+		| 100      | 5         | 95         |
+		| 1000     | 1000      | 0          |
+
+Scenario: Multiplication of two number
+	Given the first number is <firstNum>
+	When the second number is <secondNum>
+	Then the multiplication of first and second should be <mulResult>
+
+Examples: 
+		| firstNum | secondNum | mulResult |
+		| 4        | 5         | 20        |
+		| 100      | 5         | 500       |
+		| 1000     | 1000      | 1000000   |
---
+++ CalculatorTestSuite.Test/StepDefinitions/CalculatorStepDefinations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorTestSuite.Test.StepDefinitions
+{
+    [Binding]
+    internal class CalculatorStepDefinations
+    {
+        private Calculator.Calculator _calculator = new Calculator.Calculator();
+        ScenarioContext _scenarioContext;
+        public CalculatorStepDefinations(ScenarioContext context)
+        {
+            _scenarioContext = context;
+        }
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int num)
+        {
+            _scenarioContext["First"] = num;
+        }
+
+        [When(@"the second number is (.*)")]
+        public void WhenTheSecondNumberIs(int num)
+        {
+            _scenarioContext["Second"] = num;
+        }
+
+        [Then(@"the sum of first and second should be (.*)")]
+        public void ThenTheSumShouldBe(int num)
+        {
+            int result = _calculator.Add((int)_scenarioContext["First"], (int)_scenarioContext["Second"]);
+
+            Assert.IsTrue(result == num);
+        }
+
+        [Then(@"the substraction of first and second should be (.*)")]
+        public void ThenTheSubShouldBe(int num)
+        {
+            int result = _calculator.Substract((int)_scenarioContext["First"], (int)_scenarioContext["Second"]);
+
+            Assert.IsTrue(result == num);
+        }
+
+        [Then(@"the multiplication of first and second should be (.*)")]
+        public void ThenTheMulShouldBe(int num)
+        {
+            int result = _calculator.Multiply((int)_scenarioContext["First"], (int)_scenarioContext["Second"]);
+
+            Assert.IsTrue(result == num);
+        }
+
+
+    }
+}
